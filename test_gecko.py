b49cc4c54c1748914a1fd7f1247a545fb04651ee
loljack32 <67861204+loljack32@users.noreply.github.com>
2026-07-17 07:11:17 +0300

    Update test_gecko.py

--- test_gecko.py
+++ test_gecko.py
@@ -1,1 +1,18 @@
+from core.gecko import GeckoTerminal
 
+
+
+gecko = GeckoTerminal()
+
+
+
+# пример Solana пула
+
+data = gecko.get_pool_data(
+    "solana",
+    "POOL_ADDRESS"
+)
+
+
+
+print(data)
